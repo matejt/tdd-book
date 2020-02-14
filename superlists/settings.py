f933b27f0f399829c6c525c7c58302f546ac6653
DHDYK0 <matej.tacer@usps.gov>
2020-02-14 16:56:44 -0600

    add and configure fabfile.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -24,7 +24,7 @@ if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     # ALLOWED_HOSTS = [os.environ['SITENAME']]
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['56.76.62.147']
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-secret-key'
